2f432ab713005d558b642a3a8984f3c9f6cd8209
BVB09TS <bvbbvb543@gmail.com>
2026-03-08 18:23:31 +0100

    fix 1

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useTheme } from '../../contexts/ThemeContext'
 import type { Tab } from '../../types'
 
